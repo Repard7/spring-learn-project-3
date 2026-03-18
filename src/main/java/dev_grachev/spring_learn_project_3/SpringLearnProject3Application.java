a1629c4a90c77d60ac230de7a58de1b79ba6f649
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-03-18 16:13:00 +0700

    init commit

---
+++ src/main/java/dev_grachev/spring_learn_project_3/SpringLearnProject3Application.java
@@ -0,0 +1,13 @@
+package dev_grachev.spring_learn_project_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnProject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev_grachev/spring_learn_project_3/SpringLearnProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_grachev.spring_learn_project_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
